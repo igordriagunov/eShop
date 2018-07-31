d63e560cbf1a13d8e7dd9dd0cbe73c6457a1d43f
Игорь Вячеславович <work7x7@yandex.ru>
2018-07-31 14:31:37 +0300

    added Order, OrderProduct

---
+++ src/main/java/ru/itpark/entity/Order.java
@@ -0,0 +1,31 @@
+package ru.itpark.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Time;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "date")
+    private Date date;
+
+    @Column(name = "time")
+    private Time time;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.MERGE)
+    @JoinColumn(name = "account_id", foreignKey = @ForeignKey(name = "FK_accountId"))
+    private Account account;
+}
